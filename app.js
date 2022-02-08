686f44ffe685baf1b7e16cf788ca618df92b54da
bmsani <bmsani6@gmail.com>
2022-02-08 19:04:30 +0600

    js event listner

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function handleJustFunctionClick(){
+    document.getElementById('input-text').v
+
+    const p = document.getElementById('clicking-methoud');
+    
+    p.innerText = 'hi this is bm sani';
+}
